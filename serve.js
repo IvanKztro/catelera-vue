dceecddc1c3079658151f8791c06fad6a61f4c6f
Ivan Castro <ivanc654gmail.com>
2020-02-15 19:28:36 -0600

    heroku1

---
+++ serve.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const serverStatic = require("serve-static");
+const path = require("path");
+
+const app = express();
+
+app.use("/", serverStatic(path.join(__dirname, '/dist')));
+
+//const port = process.env.PORT || 8080
+app.set("port", process.env.PORT || 4000);
+
+app.listen(app.get("port"), ()=>{
+   // console.log("ESCHUANDO PUERTO: ",  app.get("port"))
+})
