8aa66dba55c6441f155b292f7ae302873e8f65c1
Ivan Castro <ivanc654gmail.com>
2020-02-18 10:17:35 -0600

    Instalacion de history api

--- serve.js
+++ serve.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const serverStatic = require("serve-static");
 const path = require("path");
+const history = require("connect-history-api-fallback");
 
 const app = express();
+app.use(history());
 
-app.use("/", serverStatic(path.join(__dirname, '/dist')));
+app.use('/',express.static(path.join(__dirname,'/dist')))
 
-//const port = process.env.PORT || 8080
 app.set("port", process.env.PORT || 4000);
 
 app.listen(app.get("port"), ()=>{
